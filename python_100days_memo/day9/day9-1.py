daf0652c62b4d951ba40e096970e46a2bc34d691
Crix Liu <crixthegreat@gmail.com>
2019-05-04 03:09:39 +0800

    Add files via upload

--- python_100days_memo/day9/day9-1.py
+++ python_100days_memo/day9/day9-1.py
@@ -20,18 +20,37 @@ class Human(object):
     def god_creat_person(cls, birthyear):
         return cls(birthyear, 'any', 'xxx')
 
+    def judge_super(self):
+        if self.birthyear < 2000 and self.sex == 'male':
+            return True
+        else:
+            return False
+
+class Superman(Human):
+
+    def __init__(self,birthyear, sex, bloodtype, name, skill):
+
+        super().__init__(birthyear, sex, bloodtype)
+        self.skill = skill
+        self.name = name
+
+    def cast(self):
+        print(self.name + ' cast skill: ' + self.skill)
+
+    def judge_super(self):
+
+        if super().judge_super():
+            print(self.name + ' is a super man and ' , end = ' ')
+            self.cast()
+        else:
+            print('no, ' + self.name + ' is not a true hero')
+
+            
 if __name__ == '__main__':
 
     he = Human(1998, 'male', 'o')
-    she = Human.god_creat_person('2999')
+    she = Human.god_creat_person(2999)
     me = Human(1981, 'male', 'ab')
-
-
-    print(he.birthyear, he.sex, he.bloodtype)
-    print(she.birthyear, she.sex, she.bloodtype)
-    print(me.birthyear, me.sex, me.bloodtype)
-
-    
     nobody = None
     
     s = input('please input the sex: ')
@@ -41,3 +60,29 @@ if __name__ == '__main__':
         print(nobody.birthyear, nobody.sex, nobody.bloodtype)
     else:
         print('there is nobody with the sex {}'.format(s))
+
+
+    onehero = Superman(1999, 'male', 'XX', 'Batman', 'GreatMuscle')
+    otherhero = Superman(1998, 'male', 'AB', 'heroine', 'TimeStop')
+
+    print(he.birthyear, he.sex, he.bloodtype, end = ' ')
+    if he.judge_super():
+        print('he is a superman')
+    else:
+        print('he is not a superman')
+
+    print(she.birthyear, she.sex, she.bloodtype, end = ' ')
+    if she.judge_super():
+        print('she is a superman')
+    else:
+        print('she is not a superman')
+
+    print(me.birthyear, me.sex, me.bloodtype, end = ' ')
+    if me.judge_super():
+        print('me is a superman')
+    else:
+        print('me is not a superman')
+    
+    onehero.judge_super()
+    otherhero.judge_super()
+    
